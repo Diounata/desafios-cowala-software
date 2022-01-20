71c1236036e605a42bc485d19565a6d2f8087a07
Diounata <jonathamcordeiro42@gmail.com>
2022-01-19 23:17:00 -0300

    Chore: add molduras bonus exercise

---
+++ Algoritmos/src/molduras.ts
@@ -0,0 +1,44 @@
+function getHorizonString(horizon: number): string {
+  if (horizon === 1) return '+';
+  else {
+    const dashAmount = horizon - 2;
+    const dashArray: string[] = [];
+
+    for (let counter = 0; counter < dashAmount; counter++) dashArray.push('-');
+
+    const dashString = Array.from(dashArray).join('');
+    const joinedString = `+${dashString}+`;
+
+    return joinedString;
+  }
+}
+
+function displayHorizonString(horizon: number): void {
+  const horizonString = getHorizonString(horizon);
+
+  console.log(horizonString);
+}
+
+function displayVerticalString(vertical: number, horizon: number): void {
+  if (horizon === 1) console.log('-');
+  else {
+    const verticalDashAmount = vertical - 2;
+    const horizonDashAmount = horizon - 2;
+    const dashArray: string[] = [];
+
+    for (let counter = 0; counter < horizonDashAmount; counter++) dashArray.push(' ');
+    const spacedString = Array.from(dashArray).join('');
+    const joinedString = `-${spacedString}-`;
+
+    for (let counter = 0; counter < verticalDashAmount; counter++) console.log(joinedString);
+  }
+}
+
+function getFrame(horizon: number, vertical: number): void {
+  displayHorizonString(horizon);
+  displayVerticalString(vertical, horizon);
+  displayHorizonString(horizon);
+}
+
+//       h, v
+getFrame(1, 3);
