55d6a45c4c22314334be20dca07ed92056439f98
Diounata <jonathamcordeiro42@gmail.com>
2022-01-21 00:00:37 -0300

    Chore: add JSX page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,35 @@ export function App() {
       </header>
 
       <main className="container">
-        <section></section>
+        <section>
+          <label>
+            Nome <input type="text" />
+          </label>
+
+          <article className="divided-input-container">
+            <label>
+              Profissão <input type="text" />
+            </label>
+
+            <label>
+              Celular <input type="tel" />
+            </label>
+          </article>
+
+          <article className="ip-input-container">
+            <label>
+              Meu IP <input type="text" disabled />
+            </label>
+
+            <button>Encontrar IP</button>
+          </article>
+
+          <footer className="button-container">
+            <button>Salvar</button>
+
+            <button>Limpar</button>
+          </footer>
+        </section>
       </main>
     </>
   );
