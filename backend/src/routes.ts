b50c115966aae52ec7b1a420221d32b1f8e714c0
Diounata <jonathamcordeiro42@gmail.com>
2022-01-22 20:44:19 -0300

    Chore: removed backend

--- backend/src/routes.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-
-const routes = Router();
-
-module.exports = routes;
--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express();
-
-const routes = require('./routes');
-
-app.use(routes);
-
-app.listen(4000, () => console.log('Server is running'));
